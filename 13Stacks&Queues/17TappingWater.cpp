5cc98d53ed3a20d9bff1df8ff3bbfcd3ee1610de
json77777 <json73747@gmail.com>
2026-05-25 20:01:50 +0530

    Add totalWater function implementation for calculating trapped water using two-pointer approach

--- 13Stacks&Queues/17TappingWater.cpp
+++ 13Stacks&Queues/17TappingWater.cpp
@@ -24,11 +24,44 @@ int tappingWater(vector<int>nums) {
     return amount;
 }
 
+int totalWater(vector<int>nums) {
+    // Two-pointer solution: track the highest wall seen so far on both sides.
+    int lMax = 0, rMax = 0, total = 0, l = 0, r = nums.size()-1;
 
+    while(l < r) {
+        // Process the side with the smaller height; that side is the limiting one.
+        if(nums[l] <= nums[r]) {
+            // Water can be trapped only if the current bar is below the left max.
+            if(lMax > nums[l]) {
+                total += lMax - nums[l];
+            }
+            else {
+                // Update left boundary when a taller bar is found.
+                lMax = nums[l];
+            }
+            l++;
+        }
+
+        else {
+            // Same logic for the right side.
+            if(rMax > nums[r]) {
+                total += rMax - nums[r];
+            }
+            else {
+                // Update right boundary when a taller bar is found.
+                rMax = nums[r];
+            }
+            r--;
+        }
+    }
+
+    return total;
+}
 
 
 int main() {
     vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
 
     cout << "total units of water trapped: " << tappingWater(height) << endl;
+    cout << "total units of water trapped: " << totalWater(height) << endl;
 }
