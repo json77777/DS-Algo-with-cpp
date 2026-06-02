78056f6ea9971efb0f5587d59eb5ef01235c5707
json77777 <json73747@gmail.com>
2026-06-02 05:15:44 +0000

    Add optimized implementation for maximum consecutive ones with at most k flips

--- 14SlidingWindow & 2 Pointers/2MaxConsecutiveOnesIII.cpp
+++ 14SlidingWindow & 2 Pointers/2MaxConsecutiveOnesIII.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int max1sBF(vector<int> nums, int k) {
     // generate the longest subarrs with k 0s
-    int c=k;
     int len = 0;
     for(int i=0; i<nums.size(); i++) {
+        int c=0;
         for(int j=i; j<nums.size(); j++) {
-            if(c && nums[j] == 0) c--;
-            else if(!c) break;
+            if(nums[j] == 0) c++;
+            if(c > k) break;
             len = max(len,j-i+1);
-        } c=k;
+        }
     }
     return len;
 }
 
-int longestOnes(vector<int>& nums, int k) {
+int longestOnes(vector<int>& nums, int k) { // TC -> O(2n) worst
     int maxLen = 0;
     int zeroes = 0;
     int l = 0, r=0;
@@ -35,9 +35,25 @@ int longestOnes(vector<int>& nums, int k) {
     return maxLen;
 }
 
+int longestOnes_Optimal(vector<int>& nums, int k) {
+    int maxLen = 0;
+    int zeroes = 0;
+    int l = 0, r=0;
+    while (r<nums.size()) {
+        if(nums[r] == 0) zeroes++;
+        if(zeroes > k) {
+            if(nums[l]==0) zeroes--;
+            l++;
+        }
+        if(zeroes <= k) maxLen = max(maxLen, r-l+1);
+        r++;
+    }
+    return maxLen;
+}
+
 int main() {
-    vector<int> nums = {1,1,1,0,0,0,1,1,1,1,0};
-    int k = 2;
+    vector<int> nums = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+    int k = 3;
     cout << "max 1s with k flips (BF): " << max1sBF(nums, k) << endl;
-    cout << "max 1s with k flips (better): " << longestOnes(nums, k) << endl;
+    cout << "max 1s with k flips (optimized): " << longestOnes(nums, k) << endl;
 }
