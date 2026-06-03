4d6249b0c99de752c12c0a9833c95407e7da4b18
json77777 <json73747@gmail.com>
2026-06-03 16:26:19 +0530

    optimal method added

--- 14SlidingWindow & 2 Pointers/3FruitsIntoBasket.cpp
+++ 14SlidingWindow & 2 Pointers/3FruitsIntoBasket.cpp
@@ -18,7 +18,7 @@ int fruitsBasket_BF(vector<int>&nums,int basket) {
 
     return len;
 }
-
+// TC -> O(2n) worst
 int fruitsBasket_Better(vector<int>&nums,int basket) {
     int len=0;
     map<int,int> mp;//(int, freq);
@@ -38,10 +38,34 @@ int fruitsBasket_Better(vector<int>&nums,int basket) {
 
     return len;
 }
+// TC -> O(n)
+int fruitsBasket_Optimal(vector<int>&nums,int basket) {
+    int len=0;
+    map<int,int> mp;//(int, freq);
+
+    int l=0,r=0;
+    while(r<nums.size()) {
+        mp[nums[r]]++;
+        if(mp.size() > basket) {
+            if(mp[nums[l]] == 0) mp.erase(nums[l]);
+            if(mp.find(nums[l]) != mp.end()) {
+                mp[nums[l]]--;
+                l++;
+            }
+            r++;
+        }
+        // find length
+        len = max(len,r-l+1);
+        r++;
+    }
+
+    return len;
+}
 
 int main() {
     int basket = 2;
-    vector<int> nums = {1, 2, 3, 2, 2};
+    vector<int> nums = {1, 2, 1};
     cout << "fruits basket(BF): " << fruitsBasket_BF(nums, basket) << endl; 
-    cout << "fruits basket(BF): " << fruitsBasket_Better(nums, basket) << endl; 
+    cout << "fruits basket(Better): " << fruitsBasket_Better(nums, basket) << endl; 
+    cout << "fruits basket(Oprimal): " << fruitsBasket_Optimal(nums, basket) << endl; 
 }
