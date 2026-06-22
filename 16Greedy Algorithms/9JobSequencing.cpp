153e972a2c4340e59cac7e1dcf7d9f879ecd5f2c
json77777 <json73747@gmail.com>
2026-06-22 10:39:01 +0000

    Add implementation for Job Sequencing problem with profit maximization

--- 16Greedy Algorithms/9JobSequencing.cpp
+++ 16Greedy Algorithms/9JobSequencing.cpp
@@ -4,28 +4,44 @@ using namespace std;
 using namespace chrono;
 
 bool cmp(const vector<int> &p, const vector<int> &q) {
-    if(p[1] == q[1]) return p[2] > q[2]; // if deadline same return acc to max Profit
-    return p[1] < q[1]; // retrun acc to min deadline
+    // sort acc to profit
+    return p[2] > q[2];
 }
 
 pair<int,int> JobSequence(vector<vector<int>> &Job) {
 
-    sort(Job.begin(), Job.end(), cmp);
-
-    // for first job
-    int c=1;
-    int profit = Job[0][2];
-    // remaining jobs
-
-    for(int i=1; i<Job.size(); i++) {
-        if(Job[i][1]  == Job[i-1][1]) continue; // same deadline
-        else {
-            c++;
-            profit+=Job[i][2];
-        }
+    int maxDeadLine = -1;
+    for(auto i: Job) {
+        maxDeadLine = max(maxDeadLine, i[1]);
     }
 
-    return {c,profit};
+    sort(Job.begin(), Job.end(), cmp);
+
+    int count=0;
+    int profit = 0;
+
+    // days
+    vector<int> slot(maxDeadLine + 1, -1);
+
+    for(int i=0; i<Job.size(); i++) {
+        if(count == maxDeadLine) break;
+        
+        // place at correct position
+
+        int j = Job[i][1];
+        while(j > 0) {
+            if(slot[j] == -1) {
+                slot[j] = Job[i][0];
+                profit+=Job[i][2];
+                count++;
+                break; // ensures no more slot is used to place the same job
+            }
+            j--;
+        }
+    }
+    
+
+    return {count,profit};
 }
 
 int main() {
@@ -39,13 +55,3 @@ int main() {
     auto end = high_resolution_clock::now();
     cout << "Time taken: " << duration_cast<milliseconds>(end - start).count() << "ms" << endl;
 }
-
-
-/*
-    2,1,10
-    3,2,40
-    4,2,30
-    1,4,20
-
-    profit=10+40+20,c=1+1+1
-*/
